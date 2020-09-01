2b95b38247afa324bd180f0c6df5f12fb179c6ad
Redingston <kondratukktc@gmail.com>
2020-09-01 13:55:15 +0300

    show picture

--- Scripter/Form1.cs
+++ Scripter/Form1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
+
 
 namespace Scripter
 {
@@ -14,7 +18,41 @@ namespace Scripter
     {
         public Form1()
         {
+            _ = GetImage();
+
             InitializeComponent();
+
+            //var src = new Mat("C:\\Users\\Endrew\\Pictures\\man.jpg", ImreadModes.Grayscale);
+            //var dst = new Mat();
+
+            //Cv2.Canny(src, dst, 50, 200);
+            ////using (new Window("src image", src))
+            ////using (new Window("dst image", dst))
+            //{
+            //    Cv2.WaitKey();
+            //}
+        }
+
+      
+        private void Button1_ClickAsync(object sender, EventArgs e)
+        {
+            GetImage();
+        }
+
+        public async Task GetImage() {
+            var png = "http://185.80.129.249:4222/getImage";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage http = await client.GetAsync(png);
+            byte[] content = await http.Content.ReadAsByteArrayAsync();
+            MemoryStream ms = new MemoryStream(content);
+            Image image = Image.FromStream(ms, true, true);
+            pictureBox1.Image = image;
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
